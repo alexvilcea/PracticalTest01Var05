b924cc18595cb5faa0b9add5c0cd6b702cbe5ed0
Alex Vilcea <alexvilcea98@gmail.com>
2021-04-15 10:31:04 +0300

    task1

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var05/views/PracticalTest01Var05MainActivity.java
@@ -0,0 +1,16 @@
+package ro.pub.cs.systems.eim.practicaltest01var05.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ro.pub.cs.systems.eim.practicaltest01var05.R;
+
+public class PracticalTest01Var05MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test01_var05_main);
+    }
+}
